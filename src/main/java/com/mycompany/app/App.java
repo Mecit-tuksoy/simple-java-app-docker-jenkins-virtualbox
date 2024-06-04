535a24a23cde70c469cc7f7834808e6c409a1081
Mecit-tuksoy <mecit.tuksoy@gmail.com>
2024-06-04 17:21:02 +0300

    first commit

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -20,7 +20,7 @@ public class App {
     static class MyHandler implements HttpHandler {
         @Override
         public void handle(HttpExchange t) throws IOException {
-            String response = "Merhaba, HTTP istegi alindi!";
+            String response = "merhaba ozgur yazilim!";
             t.sendResponseHeaders(200, response.getBytes().length);
             OutputStream os = t.getResponseBody();
             os.write(response.getBytes());
