c6acd0c6f04b97a9ccb155ffb6f109dc66757614
Mecit-tuksoy <mecit.tuksoy@gmail.com>
2024-06-09 19:40:29 +0300

    Add new Jenkinsfile

--- jenkins-gcloud-sdk-install.sh
+++ jenkins-gcloud-sdk-install.sh
@@ -45,3 +45,15 @@ echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.clou
 sudo apt-get install apt-transport-https ca-certificates gnupg
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
 sudo apt-get update && sudo apt-get install google-cloud-sdk -y
+
+
+
+#install docker
+
+
+
+
+
+sudo usermod -aG docker jenkins
+sudo systemctl restart jenkins
+
